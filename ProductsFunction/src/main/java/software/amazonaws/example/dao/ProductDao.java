0ed17b1cc38d7e8f479d87ecbbacc0c95faedc04
poprahul <poprahul@amazon.com>
2022-11-03 16:02:25 -0400

    Adding logging and tracing to products table. Changing dynamodb to have provisioned capacoty to avoid random on-demand timeout errors

--- ProductsFunction/src/main/java/software/amazonaws/example/dao/ProductDao.java
+++ ProductsFunction/src/main/java/software/amazonaws/example/dao/ProductDao.java
@@ -5,17 +5,29 @@ package software.amazonaws.example.dao;
 
 import software.amazonaws.example.entity.Product;
 import software.amazonaws.example.entity.Products;
+import software.amazon.lambda.powertools.logging.Logging;
+import software.amazon.lambda.powertools.tracing.Tracing;
 
 import java.util.Optional;
 
 public interface ProductDao {
+    @Logging
+    @Tracing
     Optional<Product> getProduct(String id);
 
+    @Logging
+    @Tracing
     void putProduct(Product product);
 
+    @Logging
+    @Tracing
     void deleteProduct(String id);
 
+    @Logging
+    @Tracing
     Products getAllProduct();
 
+    @Logging
+    @Tracing
     String createProduct(Product product);
 }
