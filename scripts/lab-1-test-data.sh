5ea1d9cdc121705e9a6faa0c3f24e54e06ef6366
poprahul <poprahul@amazon.com>
2022-11-25 13:19:06 -0500

    Removing an entry from script to match the screenshot in the workshop

--- scripts/lab-1-test-data.sh
+++ scripts/lab-1-test-data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 curl -X POST $API -H "Content-type: Application/JSON" -d '{"name":"widget", "price":15}'
 curl -X POST $API -H "Content-type: Application/JSON" -d '{"name":"bolt", "price":5}'
-curl -X POST $API -H "Content-type: Application/JSON" -d '{"name":"hex nut", "price":6}'
 curl -X POST $API -H "Content-type: Application/JSON" -d '{"name":"wrench", "price":12}'
 curl -X POST $API -H "Content-type: Application/JSON" -d '{"name":"driver", "price":4}'
 curl -X POST $API -H "Content-type: Application/JSON" -d '{"name":"socket", "price":3}'
